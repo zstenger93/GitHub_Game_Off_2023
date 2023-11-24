86d6488cff88ba0ce9c2c6a523f197ad05a9dd1e
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-11-24 12:19:03 +0100

    Movement fix

--- Scales_Of_Anubis/Scripts/Player.gd
+++ Scales_Of_Anubis/Scripts/Player.gd
@@ -5,15 +5,15 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	var speed = 100.0
 
 	if Input.is_action_pressed("move_up"):
-		velocity.y += -speed
+		velocity.y -= speed
 	if Input.is_action_pressed("move_down"):
 		velocity.y += speed
 	if Input.is_action_pressed("move_left"):
-		velocity.x += -speed
+		velocity.x -= speed
 	if Input.is_action_pressed("move_right"):
 		velocity.x += speed
 	
