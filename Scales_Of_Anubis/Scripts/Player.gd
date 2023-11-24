1832515c28a50b48f04bf1caf84f93e5618c4990
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-11-24 12:10:17 +0100

    Added input keys for the movment
    Co-Authored-By: gpasztor <118205747+bloodbine@users.noreply.github.com>

---
+++ Scales_Of_Anubis/Scripts/Player.gd
@@ -0,0 +1,21 @@
+#extends AnimatedSprite2D
+extends CharacterBody2D
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var speed = 100.0
+
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y += speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -speed
+	if Input.is_action_pressed("move_right"):
+		velocity.x += speed
+	
+	move_and_slide()
+
