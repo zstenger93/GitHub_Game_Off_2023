850f1b7b00afa5a09c8616df9a8924da48b0b4e4
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-11-25 12:42:38 +0100

    Modular code, sword inversion works

--- Scales_Of_Anubis/Scripts/Player.gd
+++ Scales_Of_Anubis/Scripts/Player.gd
@@ -9,7 +9,7 @@ var speedMP : float = 8.0
 var speedDIV : float = 3.0
 var shieldOffset : float = 12.0
 var khopeshOffset : float = 32.0
-var health : float = 50.0
+var health : float = 100.0
 var baseHealth : float = 100;
 
 func take_damage(damage):
@@ -20,11 +20,7 @@ func take_damage(damage):
 	if health <= 0:
 		get_tree().reload_current_scene()
 
-func _physics_process(_delta):
-	var speed = baseSpeed - (baseHealth - health)
-	var size = baseHealth / health;
-	sprite.scale = Vector2(size, size)
-	var totalVelocity : float = sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
+func _movment(speed, totalVelocity):
 	if velocity.x != 0:
 		if velocity.x > 0 && velocity.x < (speed / speedDIV / 2):
 			velocity.x = 0
@@ -59,14 +55,29 @@ func _physics_process(_delta):
 		if velocity.x < speed * speedMP:
 			if totalVelocity < speed * speedMP:
 				velocity.x += speed
+
+func _changeSize(size):
 	var mouse_position = get_global_mouse_position()
 	var direction = (mouse_position - sprite.global_position).normalized()
+	if size > 4:
+		size = 4
 	shield.scale = Vector2(size, size)
+	sprite.scale = Vector2(size, size)
 	khopesh.scale = Vector2(size, size)
+	var distanceFromMouse = sprite.position.distance_to(mouse_position)
 	if Input.is_action_pressed("left_click_down"):
-		shield.position  = sprite.position + direction * shieldOffset * size
-		shield.look_at(mouse_position)
+		shield.position = sprite.position + direction * shieldOffset * size
+		shield.look_at(mouse_position + direction * shieldOffset * size)
 	if Input.is_action_pressed("right_click_down"):
-		khopesh.position  = sprite.position + direction * khopeshOffset * size
-		khopesh.look_at(mouse_position)
+		khopesh.position = sprite.position + direction * khopeshOffset * size
+		khopesh.look_at(mouse_position + direction * khopeshOffset * size)
+
+func _physics_process(_delta):
+	var speed = baseSpeed - (baseHealth - health)
+	var size = 1
+	if health != 0:
+		size = baseHealth / health;
+	var totalVelocity : float = sqrt(velocity.x * velocity.x + velocity.y * velocity.y)
+	_movment(speed, totalVelocity)
+	_changeSize(size)
 	move_and_slide()
