cd4daf5b588c990763e5cc4b5f19783974c6cf04
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-11-26 12:30:33 +0100

    Animations Work + Shield

--- Scales_Of_Anubis/Scripts/Player.gd
+++ Scales_Of_Anubis/Scripts/Player.gd
@@ -7,17 +7,23 @@ extends CharacterBody2D
 var baseSpeed : float = 200.0
 var speedMP : float = 8.0
 var speedDIV : float = 3.0
-var shieldOffset : float = 12.0
-var khopeshOffset : float = 16.0
+var shieldOffset : float = 8.0
+var khopeshOffset : float = 15.0
 var health : float = 100.0
 var baseHealth : float = 100
 var damage : float = 10
 
 func take_damage(_damage):
 	health -= _damage
-	sprite.modulate = Color.RED
-	await get_tree().create_timer(0.1).timeout
-	sprite.modulate = Color.WHITE
+	if _damage != 0:
+		sprite.modulate = Color.RED
+		await get_tree().create_timer(0.1).timeout
+		sprite.modulate = Color.WHITE
+	else:
+		shield.modulate = Color.RED
+		await get_tree().create_timer(0.1).timeout
+		shield.modulate = Color.WHITE
+	
 	if health <= 0:
 		get_tree().reload_current_scene()
 
@@ -55,7 +61,20 @@ func _movment(speed, totalVelocity):
 	if Input.is_action_pressed("move_right"):
 		if velocity.x < speed * speedMP:
 			if totalVelocity < speed * speedMP:
-				velocity.x += speed
+				velocity.x += speed	
+	if (totalVelocity < 30):
+		sprite.play("idle")
+	else:
+		if (velocity.x * velocity.x > velocity.y * velocity.y):
+			if (velocity.x < 0):
+				sprite.play("left")
+			else:
+				sprite.play("right")
+		else:
+			if (velocity.y < 0):
+				sprite.play("up")
+			else:
+				sprite.play("down")
 
 func _changeSize(size):
 	var mouse_position = get_global_mouse_position()
@@ -65,10 +84,10 @@ func _changeSize(size):
 	scale = Vector2(size, size)
 	var distanceFromMouse = sprite.position.distance_to(mouse_position)
 	if Input.is_action_pressed("left_click_down"):
-		shield.position = sprite.position + direction * shieldOffset * size
+		shield.position = sprite.position + direction * shieldOffset * sqrt(size)
 		shield.look_at(mouse_position + direction * shieldOffset * size)
 	if Input.is_action_pressed("right_click_down"):
-		khopesh.position = sprite.position + direction * khopeshOffset * size
+		khopesh.position = sprite.position + direction * khopeshOffset * sqrt(size)
 		khopesh.look_at(mouse_position + direction * khopeshOffset * size)
 
 func _physics_process(_delta):
@@ -83,3 +102,12 @@ func _physics_process(_delta):
 func _on_Sword_enemy_body_entered(body):
 	if body.name.begins_with("Enemy"):
 		health += await body.take_damage(damage)
+		
+func _on_Shield_enemy_body_entered(body):
+	if body.name.begins_with("Enemy"):
+		body.Damage = 0
+
+func _on_Shield_col_area_exited(body):
+	if body.name.begins_with("Enemy"):
+		body.Damage = 5
+
