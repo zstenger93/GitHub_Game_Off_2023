2524ed69e4d1c4dc67a19c8d5cfcc34377a52054
Kārlis Vilhelms Vēbers <karlisvilhelms@gmail.com>
2023-11-29 16:45:09 +0100

    Level Changing Works

--- Scales_Of_Anubis/Scripts/GameController.gd
+++ Scales_Of_Anubis/Scripts/GameController.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var LevelTimer : Timer = get_node("/root/MainScene/Player/MapTimer")
 @onready var LevelTimeLabel : Label = get_node("/root/MainScene/Player/Camera/Canvas/MapTimerLabel")
 @export var EnemyCount : int = 0
-@export var LevelTime : float = 120
+@export var LevelTime : float = 60
 
 func _ready():
 	LevelTimer.start(LevelTime)
@@ -11,4 +11,11 @@ func _ready():
 func _process(delta):
 	LevelTimeLabel.text = str(int(LevelTimer.time_left))
 	if LevelTimer.is_stopped():
-		get_tree().reload_current_scene() #TODO: Implement level changing
+		GlobalVariable.sceneGlobal = GlobalVariable.sceneGlobal + 1
+		if GlobalVariable.sceneGlobal == 2:
+			get_tree().change_scene_to_file("res://Scenes/second_level.tscn")
+		elif GlobalVariable.sceneGlobal == 3:
+			get_tree().change_scene_to_file("res://Scenes/third_level.tscn")
+		else:
+			GlobalVariable.sceneGlobal = 2
+			get_tree().change_scene_to_file("res://Scenes/intro.tscn")
